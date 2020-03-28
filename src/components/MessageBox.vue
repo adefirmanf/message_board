c4b52067e4494182100ed832faf94b9b16dd768f
adefirmanf <syskeys32@gmail.com>
2020-03-28 15:20:14 +0700

    add messageBox component

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-card shadow="never" style="padding : 0px; width: 500px; margin-top: 10px">
+        <div slot="header">
+          <span>Message Board</span>
+        </div>
+        <el-form>
+          <el-form-item>
+            <el-input type="textarea" placeholder="Enter your message here" size="medium"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="success" size="medium" @click="submitForm('ruleForm')">Submit</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+.el-form-item {
+  margin-bottom: 15px;
+}
+</style>
