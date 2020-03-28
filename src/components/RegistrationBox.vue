8d5345d8c9f1b8dfd57285b9037911bf4ff8f72f
adefirmanf <syskeys32@gmail.com>
2020-03-28 15:21:12 +0700

    add registrationBox component

---
+++ src/components/RegistrationBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-row type="flex" class="row-bg" justify="center">
+      <el-card shadow="never" style="padding : 0px; width: 300px; margin-top: 150px">
+        <div slot="header">
+          <span>Register</span>
+        </div>
+        <el-form>
+          <el-form-item>
+            <el-input placeholder="Username" size="medium"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input placeholder="Password" size="medium"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input placeholder="Repeat Password" size="medium"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="success" size="medium" @click="submitForm('ruleForm')">Submit</el-button>
+            <el-button @click="submitForm('ruleForm')" size="medium">Back to Login</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegistrationBox"
+};
+</script>
+<style scoped>
+.el-form-item {
+  margin-bottom: 15px;
+}
+</style>
